13ff7fc8328fd1ca3d4ffe040dcfbff2aef30248
MINO <55931955+minobear@users.noreply.github.com>
2020-06-25 10:15:45 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ let isStart = false;
 let count = 0;
 let time = 10000;
 let mode = 10;
+let buttonSize = 50;
+let buttonRed = 0;
 
 function changeSec(sec){
+    let timeCount = document.getElementById("timeCount");
+
     if(isStart) return;
 
     mode = sec;
@@ -13,28 +17,46 @@ function changeSec(sec){
 }
 
 function Click(){
+    let clickCount = document.getElementById("clickCount");
+    let clickButton = document.getElementById("clickButton");
+
+    buttonSize+=0.1;
+    buttonRed+=1;
+    let buttonSizeStr = buttonSize+"px";
+
     if(startCount==4) StartCount();        
     if(time<=0 || !isStart) return;
 
-    clickCount.innerHTML = count+=1;       
+    clickCount.innerHTML = count+=1;   
+    clickButton.style.fontSize = buttonSizeStr;
+    clickButton.style.color = "rgb("+buttonRed+",0,0)";
 }
 
 function StartCount(){
+    let clickButton = document.getElementById("clickButton");
+
     if(startCount==1) {
         clickButton.innerHTML = "狂點我!";
-        isStart = true;
+        isStart = true;       
         TimeCount();   
         return;
     }
 
-    clickButton.innerHTML = startCount-=1;  
+    clickButton.style.color = "black";
+    clickButton.innerHTML = startCount-=1;     
     setTimeout(StartCount, 1000);
 }
 
-function TimeCount(){       
+function TimeCount(){ 
+    let timeCount = document.getElementById("timeCount");
+    
     time-=10; 
     timeCount.innerHTML = time <= 20 ? "0.00" : time/1000;  
-    if(time<=20) clickButton.innerHTML = "時間到!!";
+    if(time<=20) {
+        clickButton.innerHTML = "時間到!!";
+        clickButton.style.fontSize = "50px";
+        clickButton.style.color = "black"; 
+    }
     if(time>0){       
         setTimeout(TimeCount, 10);    
         return;
@@ -45,7 +67,7 @@ function TimeCount(){
 function Complate(){            
     const CPS = Math.round(count/mode * 10) / 10;
     const CPS_STR = CPS%1==0?CPS+".0":CPS;
-    const Achievement = CPS<7?"在麥塊PVP界裡, 你只是個菜鳥...":CPS<9?"在麥塊PVP界裡, 你只是個普通人和大家差不多":CPS<11?"在麥塊PVP界裡, 你比普遍的人還強了呢!":CPS<14?"在麥塊PVP界裡, 你是位中高等級的玩家!":CPS<16?"在麥塊PVP界裡, 你已經是個高手了!!":CPS<20?"OMG... 在麥塊PVP界裡, 你是位菁英玩家啊!!!":"靠杯, 這一定不是你自己點出來的=_="                 
+    const Achievement = CPS<7?"在麥塊PVP界裡, 你只是個菜鳥...":CPS<9?"在麥塊PVP界裡, 你只是個普通人和大家差不多":CPS<11?"在麥塊PVP界裡, 你比普遍的人還強了呢!":CPS<15?"在麥塊PVP界裡, 你是位中高等級的玩家!":CPS<18?"在麥塊PVP界裡, 你已經是個高手了!!":CPS<25?"OMG... 在麥塊PVP界裡, 你是位菁英玩家啊!!!":"靠杯, 這一定不是你自己點出來的=_="                 
     alert("你的CPS為: "+CPS_STR+"\n\n"+Achievement);
 
     ResetAll();
@@ -56,9 +78,11 @@ function ResetAll(){
     startCount = 4;           
     time = mode*1000;
     count = 0;  
+    buttonSize = 50;
+    buttonRed = 0;
     isStart = false;
 
     clickButton.innerHTML = "點我開始";
     clickCount.innerHTML = 0;  
-    timeCount.innerHTML = time/1000+".00";     
+    timeCount.innerHTML = time/1000+".00";  
 }
